4689772aa8ddffaebd75de9c11b69dda651a535e
noodles23 <axman@alumni.princeton.edu>
2015-10-13 05:27:07 +0800

    simple new base working

--- app/routes_old.js
+++ app/routes_old.js
@@ -2,15 +2,15 @@ var Subjects = require('./models/SubjectViews');
 
 module.exports = function(app) {
 
-  // server routes ===========================================================
-  // handle things like api calls
-  // authentication routes  
-  // sample api route
+	// server routes ===========================================================
+	// handle things like api calls
+	// authentication routes	
+	// sample api route
  app.get('/api/data', function(req, res) {
   // use mongoose to get all nerds in the database
-  Subjects.find({}, { '_id': 0, 'prod_category': 1, 'transaction_date': 1, 'sales_price': 1}, function(err, subjectDetails) {
+  Subjects.find({}, {'_id': 0, 'school_state': 1, 'resource_type': 1, 'poverty_level': 1, 'date_posted': 1, 'total_donations': 1, 'funding_status': 1, 'grade_level': 1}, function(err, subjectDetails) {
    // if there is an error retrieving, send the error. 
-       // nothing after res.send(err) will execute '_id': 0,
+       // nothing after res.send(err) will execute
    if (err) 
    res.send(err);
     res.json(subjectDetails); // return all nerds in JSON format
