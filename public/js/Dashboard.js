5c1fbac4c532e9c30a2d4e0bc7594c7c2eb66ac6
noodles23 <axman@alumni.princeton.edu>
2015-10-14 03:38:34 +0800

    3 basic charts working
    dummy 100 dataset

--- public/js/Dashboard.js
+++ public/js/Dashboard.js
@@ -9,7 +9,7 @@ function makeGraphs(error, apiData) {
 	var dateFormat = d3.time.format("%m/%d/%Y");
 	dataSet.forEach(function(d) {
 		d.transaction_date = dateFormat.parse(d.transaction_date);
-				d.transaction_date.setDate(1);
+				// d.transaction_date.setDate(1);
 		d.sales_price = +d.sales_price;
 	});
 
@@ -17,23 +17,27 @@ function makeGraphs(error, apiData) {
 	var ndx = crossfilter(dataSet);
 
 	//Define Dimensions
+	var all = ndx.groupAll();	
 	var datePosted = ndx.dimension(function(d) { return d.transaction_date; });
-	var gradeLevel = ndx.dimension(function(d) { return d.prod_category; });
-	var salesPrice = ndx.dimension(function(d) { return d.sales_price; });
+	var prodCategory = ndx.dimension(function(d) { return d.prod_catagory; });
+
+	var salespriceByDate=datePosted.group().reduceSum(dc.pluck('sales_price'));
+	var salespriceByCat=prodCategory.group().reduceSum(dc.pluck('sales_price'));
+
+	// var gradeLevel = ndx.dimension(function(d) { return d.prod_category; });
+	// var salesPrice = ndx.dimension(function(d) { return d.sales_price; });
 
 
 	//Calculate metrics
-	var projectsByDate = datePosted.group(); 
-	var projectsByGrade = gradeLevel.group(); 
-	var projectsBySalesPrice = salesPrice.group();
+	// var projectsByDate = datePosted.group(); 
+	// var projectsByGrade = gradeLevel.group(); 
+	// var projectsBySalesPrice = salesPrice.group();
 
-	var all = ndx.groupAll();
+	// var all = ndx.groupAll();
 
-	//Calculate Groups
-	var totalDonationsState = gradeLevel.group().reduceSum(function(d) {return d.sales_price;});
-
-
-	var netTotalDonations = ndx.groupAll().reduceSum(function(d) {return d.sales_price;});
+	// //Calculate Groups
+	// var totalDonationsState = gradeLevel.group().reduceSum(function(d) {return d.sales_price;});
+	// var netTotalDonations = ndx.groupAll().reduceSum(function(d) {return d.sales_price;});
 
 	//Define threshold values for data
 	var minDate = datePosted.bottom(1)[0].transaction_date;
@@ -45,32 +49,37 @@ console.log(maxDate);
     //Charts
 	var dateChart = dc.lineChart("#date-chart");
 	var resourceTypeChart = dc.rowChart("#resource-chart");
+	// var resourceTypeChart = dc.dataTable("#resource-chart");
 
 
 	dateChart
 		//.width(600)
 		.height(220)
 		.margins({top: 10, right: 50, bottom: 30, left: 50})
-		.dimension(totalDonationsState)
+		.dimension(datePosted)
 		// .group(projectsByDate)
-		.group(projectsBySalesPrice)
+		.group(salespriceByDate)
 		.renderArea(true)
 		.transitionDuration(500)
 		.x(d3.time.scale().domain([minDate, maxDate]))
 		.elasticY(true)
 		.renderHorizontalGridLines(true)
     	.renderVerticalGridLines(true)
-		.xAxisLabel("Year")
+		.xAxisLabel("Month")
 		.yAxis().ticks(6);
 
 	resourceTypeChart
         //.width(300)
         .height(220)
         // .dimension(salesPrice)
-        .dimension(totalDonationsState)
-        .group(projectsBySalesPrice)
+        .dimension(prodCategory)
+        .group(salespriceByCat)
         .elasticX(true)
         .xAxis().ticks(5);
+
+    dc.dataCount("#row-selection")
+        .dimension(ndx)
+        .group(all);
 
     dc.renderAll();
 
